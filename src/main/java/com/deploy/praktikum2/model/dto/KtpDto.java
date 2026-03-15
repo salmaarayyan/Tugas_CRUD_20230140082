01f2f68aebab6fe19f59c277a680b389934d234c
salmaarayyan <salmaarayyan@gmail.com>
2026-03-15 17:13:46 +0800

    menambahkan DTO untuk Ktp

---
+++ src/main/java/com/deploy/praktikum2/model/dto/KtpDto.java
@@ -0,0 +1,17 @@
+package com.deploy.praktikum2.model.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class KtpDto {
+
+    private Integer id;
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin;
+
+}
